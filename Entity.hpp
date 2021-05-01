747204e598d151ac42a904561667a3e2fbd8f9b2
Trimsku <trimskyroblox@gmail.com>
2021-05-01 23:41:51 +0800

    Fix small bugs

--- Entity.hpp
+++ Entity.hpp
@@ -1,3 +1,25 @@
-/*class Entity{
-    Entity(int id, sf::Text texture, )
-}*/
+
+class Entity{
+    private:
+        int id;
+        sf::Text text;
+        std::string Name;
+    Entity(int id0, std::string Name0, sf::Text texture){
+        id = id0;
+        Name = Name0;
+        text = texture;
+    }
+    public:
+    bool isFriendly;
+    int Move;
+    std::string getName(){
+        return Name;
+    }
+    int getID(){
+        return id;
+    }
+
+
+}
+
+
--- cutFunctions.cpp
+++ cutFunctions.cpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
 #include <math.h>
-int ConvertToRGB(std::string rgb){
+int ConvertToInt(std::string rgb){
     int torgb = 0;
     for(int i = 0; i < rgb.length(); i++){
-        intrgb = rgb[i] * pow(10, rgb.length() - i - 1); 
+        torgb += rgb[i] * pow(10, rgb.length() - i - 1); 
     }
+    return torgb;
 }
