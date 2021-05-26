93f45cee3a531a3f10bb84ed08d2f55bb68c5675
Trimsku <trimskyroblox@gmail.com>
2021-05-26 20:40:16 +0800

    Remove bin files in build/ folder, clean Example1 and Example2

--- build/CMakeFiles/Ascii.dir/AsciiMachineCurrent/API/src/AsciiSignal.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Ascii.dir/AsciiMachineCurrent/API/src/AsciiSignal.cpp.o: \
- /home/trimsky/AsciiMachine/AsciiMachineCurrent/API/src/AsciiSignal.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Ascii.dir/AsciiMachineCurrent/API/src/Server.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Ascii.dir/AsciiMachineCurrent/API/src/Server.cpp.o: \
- /home/trimsky/AsciiMachine/AsciiMachineCurrent/API/src/Server.cpp \
- /usr/include/stdc-predef.h \
- /home/trimsky/AsciiMachine/AsciiMachineCurrent/API/src/../include/Server.hpp
