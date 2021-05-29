215080fe78779bb6ea1d88bc2768617589ff6ba8
Trimsku <trimskyroblox@gmail.com>
2021-05-29 12:10:35 +0800

    test

---
+++ Example1/Example1.cpp
@@ -0,0 +1,25 @@
+#define GLOBAL_SIZE 50
+#include "../AsciiMachineCurrent/API/include/GUI_o.hpp"
+#include "../AsciiMachineCurrent/API/include/utils.hpp"
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1000,1000), "Ascii-G", sf::Style::Fullscreen);
+    ascii::GUI_o HelloWorld("HelloWorld");
+    HelloWorld.setGlobalXY(0,0);
+    HelloWorld.isFontLoaded("../Example1/font/10894.otf");
+    HelloWorld.newSprites("Hello World!", "HW");
+
+    while (window.isOpen()) {
+        window.clear();
+
+        EVENT;
+        EVENT_CLOSED(Q);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) window.close();
+        GUI_O_LOAD(HelloWorld, HW);
+
+        window.display();
+    }
+
+    return 0;
+}
---
+++ Example1/readme.md
@@ -0,0 +1,6 @@
+Go to AsciiMachine build directory and run this command:
+```cmake
+cmake .. -DSIMPLE_PROGRAMS ON
+make
+./AsciiSimple2
+```
