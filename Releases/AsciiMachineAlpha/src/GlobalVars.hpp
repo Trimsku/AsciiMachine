5fe4dfcc16da9cba45f4346760c28cbfe5d5010b
Trimsku <trimskyroblox@gmail.com>
2021-05-29 12:30:22 +0800

    Update stable version

--- Releases/AsciiMachineAlpha/src/GlobalVars.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-sf::RenderWindow window(sf::VideoMode(1920,1080), "test");
-#define ENABLE_GUI_BASE
-#define OBJECT_COUNT_ERROR "SIZE IS MORE THAN SIZE OF SPRITES."
-#include "Log.hpp"
-ascii::Log logs("log.txt");
